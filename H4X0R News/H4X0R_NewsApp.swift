8c1a9ad96a0a2865906d4655a5f7e1c62ac921cc
Camilo Moreno <cpinto.dev.me@gmail.com>
2021-05-23 17:42:23 -0500

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Camilo Moreno on 23/05/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
